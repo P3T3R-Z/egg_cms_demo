1013b2a2cfee51eb1c9e41ddb2107943e13e68db
zhujiayu <1611216092@qq.com>
2019-10-13 23:59:55 +0800

    管理人添加删除更新

--- app/controller/admin/manager.js
+++ app/controller/admin/manager.js
@@ -6,10 +6,19 @@ class ManagerController extends BaseController {
   async index() {
 
     // this.ctx.body='管理员列表'
+    var result=await this.ctx.model.Admin.aggregate([{
+      $lookup:{
+        from:'role',
+        localField:'role_id',
+        foreignField:'_id',
+        as: "role"
+      }
+    }])
 
+    console.log(JSON.stringify(result))
     await this.ctx.render('admin/manager/index',{
 
-      username:'张三'
+      list:result
     });
     
   } 
@@ -17,14 +26,51 @@ class ManagerController extends BaseController {
 
   async add() {
 
-
-    await this.ctx.render('admin/manager/add');
+    //获取角色类型
+    var roleresult=await this.ctx.model.Role.find()
+    await this.ctx.render('admin/manager/add',{
+      role: roleresult
+    });
     
   } 
+  async doAdd(){
+    console.log(this.ctx.request.body);
+    var addResult=this.ctx.request.body;
 
+    addResult.password=await this.service.tools.md5(addResult.password);
+
+    //判断当前用户是否存在
+
+    var adminResult=await this.ctx.model.Admin.find({"username": addResult.username});
+
+    if(adminResult.length>0){
+      await this.error('/admin/manager/add',"此管理员已经存在")
+    } else {
+
+      var admin=new this.ctx.model.Admin(addResult)
+      admin.save();
+      await this.success('/admin/manager/add',"增加用户成功")
+    }
+
+
+
+    
+   
+    
+  }
   async edit() {
-
-    await this.ctx.render('admin/manager/edit');
+ 
+    //获取编辑的数据
+    var id=this.ctx.request.query.id;
+    var adminResult=await this.ctx.model.Admin.find({
+      _id: id
+    })
+    var role=await this.ctx.model.Role.find()
+    
+    await this.ctx.render('admin/manager/edit',{
+      adminResult,
+      role
+    });
   } 
 }
 
